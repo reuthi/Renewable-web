cdedfb81d08b5a8a6894c5b4a9f6f68bb9724c95
Reut <reut@soficoop.com>
2020-01-28 09:46:10 +0200

    con

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+
 const app = express();
 // app.use(favicon(__dirname + '/public/favicon.png'));
 // the __dirname is the current directory from where the script is running
@@ -11,3 +12,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 app.listen(port);
+console.log('listenign')
